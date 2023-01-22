ad0d377c309bcc28423fb606a788570680b215ce
Vikas Chaudhary <vikaschaudhary8286@gmail.com>
2023-01-22 16:30:03 +0530

    Refactoring the current widget. Like I added the text inside a Card

--- lib/main.dart
+++ lib/main.dart
@@ -37,18 +37,38 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
           Text('A GREAT random idea:'),
-          Text(appState.current.asLowerCase),
+          BigCard(pair: pair),
 
           ElevatedButton(onPressed: (){
             appState.getNext();
           }, child: Text('Next'),
           )
         ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    Key? key,
+    required this.pair,
+  }) : super(key: key);
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(pair.asUpperCase),
       ),
     );
   }
