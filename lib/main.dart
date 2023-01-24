46516e992e36c17fc8f364471e0c3c77ba0159dd
Vikas Chaudhary <vikaschaudhary8286@gmail.com>
2023-01-24 14:54:39 +0530

    Added like button to the UI

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,20 @@ class MyApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
   void getNext() {
+
     current = WordPair.random();
+    //notify listner, tell all other objects about the change happening in this context
+    notifyListeners();
+  }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
     notifyListeners();
   }
 }
@@ -39,6 +52,13 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
+    IconData icon;
+    if(appState.favorites.contains(pair)){
+      icon = Icons.favorite;
+    }else{
+      icon = Icons.favorite_border;
+    }
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -47,10 +67,25 @@ class MyHomePage extends StatelessWidget {
             
             BigCard(pair: pair),
             SizedBox(height: 15),
+            //added like button with  heart(like) icon
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(onPressed: (){
+                    appState.toggleFavorite();
+                }, 
+                icon: Icon(icon),
+                label: Text('Like')),
+                
 
-            ElevatedButton(onPressed: (){
-              appState.getNext();
-            }, child: Text('Next'),
+                ElevatedButton(onPressed: (){
+                  appState.getNext();
+                }, child: Text('Next'),
+                ),
+
+                
+                
+              ],
             )
           ],
         ),
